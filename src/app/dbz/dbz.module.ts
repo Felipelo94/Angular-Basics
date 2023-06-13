1d3961ddcc71c9fe9a22b29d4f4d7a94eec1b738
Felipe Londono <felipe.londono@pragma.com.co>
2023-06-13 12:26:40 -0500

    avances del curso

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DbzMainPageComponent } from './pages/main-page.componten';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [DbzMainPageComponent, ListComponent, AddCharacterComponent],
+  exports: [DbzMainPageComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class DbzModule {}
