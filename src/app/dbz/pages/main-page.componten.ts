0cd6ed253dcad996135297068b5e202c25180676
Felipe Londoño <juan.londono@grupomutual.fi.cr>
2023-06-29 22:33:47 -0500

    finish the proyect

--- src/app/dbz/pages/main-page.componten.ts
+++ src/app/dbz/pages/main-page.componten.ts
@@ -1,28 +1,27 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 export class DbzMainPageComponent {
-  characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9500,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500,
-    },
-  ];
-
-  onNewCharacter(character: Character) {
-    console.log(character);
-    console.log('Main page');
+  
+  constructor( private dbzService: DbzService) {
+    
   }
+
+  get characters () : Character[] {
+    return [...this.dbzService.characters]
+  }
+
+  onDeleteCharacter(id:string) : void {
+    this.dbzService.deleteCharacterById(id)
+  }
+
+  onNewCharacter(character: Character) : void {
+    this.dbzService.addCharacter(character)
+  }
+
 }
